de7ca5b25d8915d90dae2140932a79a93c3cea22
Jedidah Kinuthia <kinuthiaj.wahu@gmail.com>
2016-11-02 16:25:07 +0300

    Add files via upload

---
+++ Missing number.py
@@ -0,0 +1,11 @@
+def missing_number(array1, array2):
+	array1.sort()
+	array2.sort()
+	for i in array2:
+		if i > number:
+			number = i
+	return number
+	for i in array1:
+	  if i > integer:
+	    number =i
+	return integer
